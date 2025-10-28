79b8e0a34c0eea741d3a2f7b3364623f8bbbdd44
rasha <aysharasha258@gmail.com>
2025-10-28 17:34:59 +0530

    small update

--- service/auth.js
+++ service/auth.js
@@ -2,8 +2,9 @@ import jwt from "jsonwebtoken";
 
 
 export default function Auth(req,res,next) {
-    const authToken = req.cookies?.authToken;
-    console.log("authToken",authToken)
+    const authHeader = req.headers.authorization;
+    const authToken = authHeader?.split(" ")[1];
+      console.log("authToken",authToken)
     if (!authToken) {
         return res.status(401).json({ message: "Unauthorized access" });
       }
